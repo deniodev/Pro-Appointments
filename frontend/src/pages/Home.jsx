4ae6f3b3db81d5792219504295aaff5d7a53e337
Denio Nhanale <dnhanale@gmail.com>
2024-02-02 10:41:29 +0200

    Add Services Section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,6 +8,7 @@ import icon03 from '../assets/images/icon03.png';
 import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
 import About from '../components/About/About';
+import ServiceList from '../components/Services/ServiceList';
 
 const Home = () => { 
   return  <> 
@@ -170,6 +171,20 @@ const Home = () => {
   </section>
   
   <About />
+
+  {/* ============== services section ============ */}
+  <section>
+    <div className="container">
+      <div className="xl:w-[470px] mx-auto">
+        <h2 className='heading text-center'>Our services</h2>
+        <p className='text__para text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+          Facilis quasi molestia</p>
+      </div>
+
+      <ServiceList/>
+    </div>
+  </section>
+  {/* ============== services section ============ */}
   </>
 }
 
