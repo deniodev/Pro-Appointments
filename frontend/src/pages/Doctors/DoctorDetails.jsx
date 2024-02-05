6ec6edd7d632aed0df9b4dab0ee549fb0cd23dc7
Denio Nhanale <dnhanale@gmail.com>
2024-02-05 10:56:10 +0200

    Add Doctors Details Sidebar

--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -3,6 +3,7 @@ import doctorImg from '../../assets/images/doctor-img02.png';
 import starIcon from '../../assets/images/Star.png';
 import DoctorAbout from './DoctorAbout';
 import Feedback from './Feedback';
+import SidePanel from './SidePanel';
 
 const DoctorDetails = () => {
 
@@ -65,12 +66,11 @@ const DoctorDetails = () => {
                 {tab==='about' && <DoctorAbout />}
                 {tab==='feedback' && <Feedback />}
               </div>
-
-
-
           </div>
 
-          <div></div>
+          <div>
+            <SidePanel />
+          </div>
         </div>
       </div>
     </section>
