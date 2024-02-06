44f7dfad424f54f1bb2edffa1d3cea60c9b21183
Denio Nhanale <dnhanale@gmail.com>
2024-02-06 15:22:34 +0200

    Connect to MongoDB Database

--- backend/index.js
+++ backend/index.js
@@ -18,11 +18,28 @@ app.get('/', (req,res)=>{
     res.send('Api is working');
 });
 
+// database connection
+mongoose.set('strictQuery', false)
+const connectionDB = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        console.log('MongoDB database is connected')
+
+    } catch (err) {
+        console.log('MongoDB database connection failed')
+    }
+}
+
 // middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
 app.listen(port, ()=>{
+    connectionDB();
     console.log('Server is running on port' + port);
 })
