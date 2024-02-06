6e5cef964c5c74058d443afab38cfc53d682dc6c
Denio Nhanale <dnhanale@gmail.com>
2024-02-06 14:54:50 +0200

    Setup the server with express

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 8000
+
+const corsOptions = {
+    origin:true
+};
+
+app.get('/', (req,res)=>{
+    res.send('Api is working');
+});
+
+// middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+
+app.listen(port, ()=>{
+    console.log('Server is running on port' + port);
+})
