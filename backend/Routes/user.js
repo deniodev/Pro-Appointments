f6f2b73e8180fddb166a40dd79c35784cf27e97f
Denio Nhanale <dnhanale@gmail.com>
2024-02-07 16:36:41 +0200

    Setup the Authentication

--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { updateUser, deleteUser, getAllUser, getSingleUser } from "../Controllers/userController.js";
+import { authenticate, restrict } from "../auth/verifyToken.js"; 
 
 const router = express.Router()
 
-router.get('/:id', getSingleUser)
-router.get('/', getAllUser)
-router.put('/:id', updateUser)
-router.delete('/:id', deleteUser)
+router.get('/:id', authenticate, restrict(["patient"]), getSingleUser)
+router.get('/', authenticate, restrict(["admin"]), getAllUser)
+router.put('/:id',  authenticate, restrict(["patient"]), updateUser)
+router.delete('/:id',  authenticate, restrict(["admin"]), deleteUser)
 
 export default router;
