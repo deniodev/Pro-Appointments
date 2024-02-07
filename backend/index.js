38b050bb98216bbbc5e1b13e4a9482ebded6ba55
Denio Nhanale <dnhanale@gmail.com>
2024-02-07 11:53:51 +0200

    Add User Routes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import authRoute from "./Routes/auth.js"
+import authRoute from "./Routes/auth.js";
+import userRoute from "./Routes/user.js";
 
 
 dotenv.config()
@@ -40,6 +41,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/users', userRoute);
+
 
 app.listen(port, ()=>{
     connectionDB();
