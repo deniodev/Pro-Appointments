5a596d72c109d0ed03dbceea715f4c5b0503897b
Denio Nhanale <dnhanale@gmail.com>
2024-02-09 10:18:02 +0200

    Add Protected Route to Routers

--- frontend/src/routes/ProtectedRoute.jsx
+++ frontend/src/routes/ProtectedRoute.jsx
@@ -3,11 +3,11 @@ import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
 import { authContext } from '../context/AuthContext';
 
-const ProtectedRoute = (children, alloedRoles) => {
+const ProtectedRoute = ({children, allowedRoles}) => {
   
     const {token, role} = useContext(authContext);
 
-    const isAllowed = alloedRoles.includes(role);
+    const isAllowed = allowedRoles.includes(role);
     const accessibleRoute = token && isAllowed ? children : <Navigate to='/login' replace={true}/>
 
     return accessibleRoute;
--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -10,6 +10,7 @@ import MyAccount from '../Dashboard/user-account/MyAccount';
 import  Dashboard  from '../Dashboard/doctor-account/Dashboard';
 
 import {Routes, Route} from 'react-router-dom';
+import ProtectedRoute from './ProtectedRoute';
 
 const Routers = () => {
   return (
@@ -22,8 +23,8 @@ const Routers = () => {
     <Route path='/register' element={<Signup />} />
     <Route path='/contact' element={<Contact />} />
     <Route path='/services' element={<Services />} />
-    <Route path='/users/profile/me' element={<MyAccount />} />
-    <Route path='/doctors/profile/me' element={<Dashboard />} />
+    <Route path='/users/profile/me' element={<ProtectedRoute allowedRoles={['patient']}><MyAccount /></ProtectedRoute>} />
+    <Route path='/doctors/profile/me' element={<ProtectedRoute allowedRoles={['doctor']}><Dashboard /></ProtectedRoute>} />
   </Routes>
   );
 };
