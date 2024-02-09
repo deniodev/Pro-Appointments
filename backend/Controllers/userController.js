af64bd2148b0ef02869fb815acc0dd489df7069f
Denio Nhanale <dnhanale@gmail.com>
2024-02-09 09:22:16 +0200

    Update User Controller to get the User Profile

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,4 +1,6 @@
 import User from"../models/UserSchema.js";
+import Booking from "../models/BookingSchema.js";
+import Doctor from "../models/DoctorSchema.js";
 
 export const updateUser = async(req,res)=>{
     const id = req.params.id;
@@ -73,3 +75,40 @@ export const getAllUser = async(req,res)=>{
         res.status(404).json({ success:false, message:'Not found'})
     }
 };
+
+export const getUserProfile = async(req,res)=>{
+    const userId = req.userId
+
+    try {
+        const user = await User.findById(userId);
+
+        if(!user){
+            return res.status(404).json({success:false, message:'User not found'})
+        }
+
+        const {password, ...rest} = user._doc;
+
+        res.status(200).json({success:true, message:'Profile info is getting', data:{...rest}})
+
+    } catch (err) {
+        res.status(500).json({ success:false, message:'Something went wrong, cannot get'});
+    }
+};
+
+export const getMyAppointments = async(req,res)=>{
+    try {
+        //retrieve appointments from booking
+        const bookings  = await Booking.find({user:req.userId});
+
+        //extract doctor ids from appointment bookings
+        const doctorIds = bookings.map(el=>el.doctor.id);
+
+        //retrieve doctors using doctor ids
+        const doctors = await Doctor.find({_id: {$in:doctorIds}}).select('-password');
+
+        res.status(200).json({success:true, message:'Appointments are getting', data:doctors});
+        
+    } catch (err) {
+        res.status(500).json({ success:false, message:'Something went wrong, cannot get'});
+    }
+}
