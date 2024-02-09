e202251e7648f58de2d06c9f75da27054423855d
Denio Nhanale <dnhanale@gmail.com>
2024-02-09 09:40:04 +0200

    Update Doctor Route to get Doctor Profile & Appointments

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { updateDoctor, deleteDoctor, getAllDoctor, getSingleDoctor } from "../Controllers/doctorController.js";
+import { updateDoctor, deleteDoctor, getAllDoctor, getSingleDoctor, getDoctorProfile } from "../Controllers/doctorController.js";
 import { authenticate, restrict } from "../auth/verifyToken.js"; 
 import reviewRouter from "./review.js";
 
@@ -11,6 +11,8 @@ router.use('/:doctorId/reviews', reviewRouter);
 router.get('/:id', getSingleDoctor)
 router.get('/', getAllDoctor)
 router.put('/:id', authenticate, restrict(["doctor"]), updateDoctor);
-router.delete('/:id',  authenticate, restrict(["doctor"]), deleteDoctor)
+router.delete('/:id',  authenticate, restrict(["doctor"]), deleteDoctor);
+
+router.get('/profile/me', authenticate, restrict(['doctor']), getDoctorProfile);
 
 export default router;
