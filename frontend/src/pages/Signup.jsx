75327ae4ba1eece2bcaab3be16abf790accb1a35
Denio Nhanale <dnhanale@gmail.com>
2024-02-10 11:59:25 +0200

    Fetch user profile data to populate the User Profile Page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import signupimg from '../assets/images/signup.gif';
-import avatar from '../assets/images/doctor-img01.png';
 import { Link, useNavigate } from 'react-router-dom';
 import uploadImageToCloudinary from '../utils/uploadCloudinary';
 import { BASE_URL } from '../config';
