95fee9e095af758c93fed3b15b23ce2d7b3a7bde
Denio Nhanale <dnhanale@gmail.com>
2024-02-12 18:39:23 +0200

    Add Profile tab to the Doctor Page

--- frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -6,6 +6,7 @@ import { BASE_URL } from '../../config';
 import Tabs from './Tabs';
 import starIcon from '../../assets/images/Star.png';
 import DoctorAbout from '../../pages/Doctors/DoctorAbout';
+import Profile from './Profile';
 
 const Dashboard = () => {
 
@@ -59,25 +60,26 @@ const Dashboard = () => {
                           <div className="">
                             <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded
                             text-[12px] leading-4 lg:text-[16px] lg:leading-6 font-semibold'>
-                              Surgeon
+                              {data.specialization}
                             </span>
 
                             <h3 className="text-[22px] leading-9 font-bold text-headingColor mt-3">
-                              Olalilas
+                              {data.name}
                             </h3>
                             <div className="flex items-center gap-[6px]">
                               <span className="flex items-center gap-[6px] text-headingColor text-[14px]
                               leading-5 lg:text-[16px] lg:leading-6 font-semibold">
-                                <img src={starIcon} alt="" />4.5
+                                <img src={starIcon} alt="" />
+                                {data.averageRating}
                               </span>
                               <span className="text-textColor text-[14px]
                               leading-5 lg:text-[16px] lg:leading-6 font-semibold">
-                                (233)
+                                ({data.totalRating})
                               </span>
                             </div>
 
                             <p className="text__para font-[15px] lg:max-w-[390px] leading-6">
-                              doctor bio
+                              {data?.bio}
                             </p>
                           </div>
                       </div>
@@ -91,7 +93,7 @@ const Dashboard = () => {
 
 
                   {tab === 'appointments'  && <div>appointments</div>}
-                  {tab === 'settings'  && <div>profile settings</div>}
+                  {tab === 'settings'  && <Profile />}
                 </div>
 
             </div>
