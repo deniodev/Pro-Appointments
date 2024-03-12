7e58b136520e2422cd8ddad2e65310ff6743fd30
Denio Nhanale <dnhanale@gmail.com>
2024-03-12 09:16:35 +0200

    Setup Filter Function

--- frontend/src/pages/Pros/Pros.jsx
+++ frontend/src/pages/Pros/Pros.jsx
@@ -8,12 +8,7 @@ import { useEffect, useState } from 'react';
 const Pros = () => {
   const [query, setQuery] = useState('');
   const [debounceQuery, setDebounceQuery] = useState("");
-
-  const handleSearch = () => {
-    setQuery(query.trim())
-
-    console.log('handle search');
-  };
+  const [selectedCategory, setSelectedCategory] = useState('All');
 
   useEffect(()=>{
 
@@ -25,7 +20,12 @@ const Pros = () => {
 
   },[query])
 
-  const { data: pros, loading, error } = useFetchData(`${BASE_URL}/pros?query=${debounceQuery}`)
+  const { data: pros, loading, error } = useFetchData(`${BASE_URL}/pros?query=${debounceQuery}`);
+
+  const filteredPros = selectedCategory === 'All'
+  ? pros
+  : pros.filter(pro => pro.specialization === selectedCategory);
+
   return (
   <>
 
@@ -33,32 +33,36 @@ const Pros = () => {
         <div className="max-w-[400px] mt-[10px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between">
             <input 
             type="search"
-            className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer'
-            placeholder='Pesquise pelo nome'
+            className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-text'
+            placeholder='Pesquisar'
             value={query}
             onChange={e=> setQuery(e.target.value)}
             />
 
-            <select 
-              className=' btn appearance-none mt-0 rounded-[0px] rounded-r-md cursor-pointer' 
-                    onChange={(e) => filterByCategory(e.target.value)}
-                >
-                    <option value="All">Categorias</option>
-                    <option value="Category 1">Design</option>
-                    <option value="Category 2">Eventos</option>
-                </select>
+            <select
+            className="btn appearance-none mt-0 rounded-[0px] rounded-r-md cursor-pointer pl-4 pr-4"
+            value={selectedCategory}
+            onChange={e => setSelectedCategory(e.target.value)}
+            >
+              <option value="All">Todas Categorias</option>
+              <option value="Assistência Técnica">Assistência Técnica</option>
+              <option value="Aulas">Aulas</option>
+              <option value="Design e Tecnologia">Design e Tecnologia</option>
+              <option value="Eventos">Eventos</option>
+              <option value="Reformas">Reformas</option>
+              <option value="Serviços Domésticos">Serviços Domésticos</option>
+          </select>
         </div>
     </div>
 
 
   <section>
     <div className="container">
-
     {loading && <Loader/>}
     {error && <Error/>}
       {!loading && !error && (
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
-        {pros.map((pro)=> (
+        {filteredPros.map((pro)=> (
              <ProCard key={pro.id} pro={pro}/>
         ))}
       </div>
