33373ca8457a127cb1ab373751a2db7192e4234d
Denio Nhanale <dnhanale@gmail.com>
2024-03-16 10:39:08 +0200

    Remove the Translation Icon

--- frontend/src/components/LanguageSwitcher/index.jsx
+++ frontend/src/components/LanguageSwitcher/index.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import lang from "../../assets/images/language.png"
 
 const languageOptions = [
     {
@@ -20,12 +21,11 @@ export const LanguageSwitcher = () => {
     };
 
     return (
-        <div className="language-switcher">
-            {/* <span>{t("selectLanguage")}</span> */}
-
+        <div className="flex">
+            {/* <img src={lang} alt="" className='w-[27px] h-[27px] rounded-full mr-2' /> */}
             <select 
             onChange={handleLanguageChange}
-            className="cursor-pointer text-[20px] leading-7 font-[600] appearance-none"
+            className="cursor-pointer text-[20px] leading-7 font-[800] appearance-none hover:bg-[#0066ff2c] rounded-md"
             >
                 {languageOptions.map((languageOption) => (
                     <option
