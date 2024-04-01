8c1f1c918252aff101238dd42633882096756ba2
Denio Nhanale <dnhanale@gmail.com>
2024-04-01 15:01:03 +0200

    Update the Logo

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useContext } from "react";
-import logo from "../../assets/images/logo.png";
+import logo from "../../assets/images/logo2.png";
 import { NavLink, Link } from "react-router-dom";
 import { BiMenu } from "react-icons/bi";
 import { authContext } from "../../context/AuthContext";
