cc091839d644a24ff5f8d6d140a68e220e11a23a
Denio Nhanale <dnhanale@gmail.com>
2024-04-04 14:41:39 +0200

    Refactoring token

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,2 @@
-let token = null;
-
-// Check if localStorage is available before accessing it
-if (typeof localStorage !== 'undefined') {
-    token = localStorage.getItem("token");
-}
-
 export const BASE_URL = "https://profinderapi.onrender.com/api/v1";
-export { token };
+export const token = localStorage.getItem('token');
