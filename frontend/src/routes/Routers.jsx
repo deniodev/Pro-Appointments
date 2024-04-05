5687f59fdbec46522ae260b9c25568594a57457f
Denio Nhanale <dnhanale@gmail.com>
2024-04-05 07:51:06 +0200

    Refactoring

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -6,7 +6,7 @@ import Signup from '../pages/Signup';
 import Contact from '../pages/Contact';
 import MyAccount from '../Dashboard/user-account/MyAccount';
 import  Dashboard  from '../Dashboard/pro-account/Dashboard';
-import {Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import ProtectedRoute from './ProtectedRoute';
 import Pros from '../pages/Pros/Pros';
 import ProDetails from '../pages/Pros/ProDetails';
@@ -22,8 +22,20 @@ const Routers = () => {
     <Route path='/register' element={<Signup />} />
     <Route path='/contact' element={<Contact />} />
     <Route path='/services' element={<Services />} />
-    <Route path='/users/profile/me' element={<ProtectedRoute allowedRoles={['client']}><MyAccount /></ProtectedRoute>} />
-    <Route path='/pros/profile/me' element={<ProtectedRoute allowedRoles={['pro']}><Dashboard /></ProtectedRoute>} />
+    <Route 
+      path='/users/profile/me' 
+      element={
+      <ProtectedRoute allowedRoles={['client']}>
+        <MyAccount />
+      </ProtectedRoute>
+      } />
+    <Route 
+      path='/pros/profile/me' 
+      element={
+      <ProtectedRoute allowedRoles={['pro']}>
+        <Dashboard />
+      </ProtectedRoute>
+      } />
   </Routes>
   );
 };
